df41471a4252b22d1d59cdcbb7f3926f11fe50a1
Andrea Poggi <andrea.poggi@softeco.it>
2015-09-04 10:20:52 +0200

    - Check for correct JSON in --params field - When a config file is provided:   1) Check if it exists   2) Check if it is correctly formatted as a YAML file   3) If it contains Scheduler params, use them to overwrite the ones provided at creation time

--- src/Mir/lib/Mir/Acq/Scheduler.pm
+++ src/Mir/lib/Mir/Acq/Scheduler.pm
@@ -67,6 +67,9 @@ use Queue::Q::ReliableFIFO::Redis   ();
 use Mir::Config::Client             ();
 use Log::Log4perl;
 use Getopt::Long                    qw( GetOptions );
+use JSON;
+use YAML                            qw(LoadFile);
+use TryCatch;
 
 my $config;
 my $queues = {};
@@ -87,6 +90,7 @@ has 'campaigns'     => ( is => 'rw', isa => 'ArrayRef', trigger => \&_set_queue 
 has 'fetchers'      => ( is => 'rw', isa => 'ArrayRef' );
 has 'processors'    => ( is => 'rw', default => 1 );
 has 'params'        => ( is => 'rw', isa => 'Str' );
+has 'config_file'   => ( is => 'rw', isa => 'Str' );
 has 'queues'        => ( is => 'ro', isa => 'HashRef' );
 
 sub _set_queue {
@@ -140,6 +144,45 @@ sub parse_input_params {
     $self->processors( $processors )    if ( defined $processors );
     $self->params( $params )            if ( defined $params );
     $self->config_file( $config_file )  if ( defined $config_file );
+
+    # If we only have fetcher configured processors must be 
+    # set to 1 by default
+#    if (defined $self->{fetchers} && scalar @{$self->{campaigns}} == 0) {
+#        $self->processors(1);
+#    }
+
+    # If params is defined, check whether it is a valid JSON
+    # string
+    if (defined $self->{params}) {
+        my $json = JSON->new->allow_nonref;
+        try {
+            my $h = $json->decode ( $self->{params} );
+        } catch ($err) {
+            $self->log->error("Invalid JSON string in params field: ".$err);
+            return undef;
+        }
+    }
+
+    # Load config file if provided. Test its existence first
+    if (defined $self->{config_file}) {
+        if (not stat ($self->{config_file})) {
+            $self->log->error("Config file ".($self->{config_file})." does not exist");
+            return undef;
+        }
+        try {
+            # Overwrite class attributes, if defined within file
+            my $config = LoadFile($self->{config_file});
+            my %attrs = map{_get_attr_name($_->name) => $_->get_write_method} __PACKAGE__->meta->get_all_attributes;
+            for my $config_param (keys %$config) {
+                if ((grep {$_ eq $config_param} keys %attrs) && $config_param ne 'config_file' ) {
+                    $self->_set_param($config_param, $config->{$config_param}, $attrs{$config_param});
+                }
+            }
+        } catch ($err) {
+            $self->log->error("Cannot read config file ".($self->{config_file})." : ".$err);
+            return undef;
+        }
+    }
 
     return 1;
 }
@@ -237,6 +280,39 @@ in case of errors.
 #=============================================================
 sub fork_processors {
     my $self = shift;
+}
+
+# Check class attribute name and check its correspondence with
+# config params names
+sub _get_attr_name {
+    my $attr_name = shift;
+
+    if ($attr_name eq 'campaigns' || $attr_name eq 'fetchers') {
+        return (substr($attr_name, 0, length($attr_name) - 1));
+    } else {
+        return $attr_name;
+    }
+}
+
+# Set params of class, when defined within configuration file
+sub _set_param {
+    my ($self, $param, $value, $write_method) = @_;
+
+    if (defined $write_method) {
+        # If param was intended to be an array, cover all possible cases
+        if (ref $self->{$write_method} eq 'ARRAY') {
+            if (ref $value eq 'ARRAY') {
+                $self->{$write_method} = $value;
+            } else {
+                $self->{$write_method} = [$value];
+            }
+        } else {
+            $self->{$write_method} = $value;
+        }
+    } else {
+        $self->log->debug("Cannot write $param, it is a read only param");
+    }
+
 }
 
 1;
