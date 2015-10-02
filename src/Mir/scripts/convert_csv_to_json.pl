baa06a1e1978acc8e4f0c20321c166b5498d5c71
Andrea Poggi <andrea.poggi@softeco.it>
2015-10-02 10:29:26 +0200

    - YAML file can be provided to map input fields to some output structure

--- src/Mir/scripts/convert_csv_to_json.pl
+++ src/Mir/scripts/convert_csv_to_json.pl
@@ -4,19 +4,21 @@
 #
 #        USAGE: ./convert_csv_to_json.pl  --file <csv input file> 
 #                       --tag <JSON tag> --separator <CSV field separator> 
-#                       --output <output file>
+#                       --mapfile <YML mapping file> --output <output file>
 #
 #  DESCRIPTION: Converts CSV data to JSON structure. Source data must be 
-#               formatted as a CSV file, with semi-colon (:) as separator
+#               formatted as a CSV file. This version currently processes files
+#               that contains labels in first column.          
 #
 #      OPTIONS: --file <csv input file>     The source file (mandatory)
 #               --tag                       JSON tag for output structure 
 #                                           (optional)
 #               --separator                 CSV field separator (optional. If
 #                                           not defined, comma will be used)                
+#               --mapfile                   YAML map file (optional)
 #               --output <outfile>          The output file (optional. If not
 #                                           defined, STDOUT will be used)
-# REQUIREMENTS: JSON, Getopt::Long
+# REQUIREMENTS: JSON, Getopt::Long, YAML, Log::Log4perl
 #         BUGS: ---
 #        NOTES: ---
 #       AUTHOR: Andrea Poggi
@@ -32,25 +34,36 @@ use utf8;
 
 use JSON;
 use Getopt::Long                    qw( GetOptions );
+use YAML                            qw( Load );
 use Log::Log4perl qw(:easy);
 Log::Log4perl->easy_init( $DEBUG );
 my $log = Log::Log4perl->get_logger( __PACKAGE__ );
 
-my ($file, $tag, $separator, $output) = (undef, undef, ",", undef);
+my ($file, $tag, $separator, $mapfile, $output) = (undef, undef, ",", undef, undef);
 GetOptions (
     "file=s"        => \$file,
     "tag=s"         => \$tag,
     "separator=s"   => \$separator,
+    "mapfile=s"     => \$mapfile,
     "output=s"      => \$output,
 ) or die <<EOT;
 
-Usage: $0 --file <csv input file> --tag <optional JSON tag> --separator <optional CSV field separator> --output <optional output file, STDOUT if not provided>
+Usage: $0 --file <csv input file> --tag <optional JSON tag> --separator <optional CSV field separator> --separator <separator char> --mapfile <mapfile path> --output <optional output file, STDOUT if not provided>
 
 EOT
 
 die ("CSV file has to be passed via the --file input param\n") unless $file;
 
 my $rows = [];
+my $item;
+
+if (defined $mapfile) {
+    $log->debug("Mapping file $mapfile was provided, loading it...");
+    open FILE_INFO, "< $mapfile" or die "Cannot open mapping file $mapfile";
+    read (FILE_INFO, $item, (stat(FILE_INFO))[7]);
+    close FILE_INFO;
+    $log->debug("Mapping file $mapfile was correctly loaded");
+}
  
 # Get all rows and put them in an array of hashes
 $log->debug("Opening $file...");
@@ -71,14 +84,43 @@ while ( my $row = <$fh> ) {
             }
         }
         if ($first_row) {
-           push @$rows, { $label => $cell }; 
+            # If mapping was provided, use it
+            if (defined $item) {
+                my $single_row = $item;
+                # Labels in mapping structure are uppercase and with _ instead of spaces
+                $label =~ s/\s+/_/g;
+                $label = uc $label;
+                $single_row =~ s/$label/$cell/g;
+                push @$rows, $single_row;
+            } else {           
+                push @$rows, { $label => $cell }; 
+            }
         } else {
-           $rows->[$index++]->{$label} = $cell;
+            # If mapping was provided, use it
+            if (defined $item) {
+                my $single_row = $rows->[$index];
+                # Labels in mapping structure are uppercase and with _ instead of spaces
+                $label =~ s/\s+/_/g;
+                $label = uc $label;
+                $single_row =~ s/$label/$cell/g;
+                $rows->[$index++] = $single_row;
+            } else {
+               $rows->[$index++]->{$label} = $cell;
+            }
         }
     }
     $first_row = undef;
 }
 close $fh;
+
+# If mapping was provided, we need to convert YAML code to hashes before encoding to JSON
+if (defined $item) {
+    for (my $index = 0; $index < scalar @$rows; $index++) {
+        my $single_row = $rows->[$index];
+        $single_row = Load($single_row);
+        $rows->[$index] = $single_row;
+    }
+}
 
 # Convert data to JSON
 if (scalar @$rows > 0) {
@@ -104,3 +146,4 @@ if (scalar @$rows > 0) {
 } else {
     $log->error("Something may have gone wrong, $file contains no cells");
 }
+
