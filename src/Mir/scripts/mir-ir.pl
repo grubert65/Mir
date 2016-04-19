a2a6228b85a0276436cf3546cd902e9b0986b372
Marco Masetti <grubert65@gmail.com>
2016-04-19 13:54:58 +0200

    Log::LogRotate configured

--- src/Mir/scripts/mir-ir.pl
+++ src/Mir/scripts/mir-ir.pl
@@ -7,7 +7,8 @@
 #                               --config_driver <a Mir::Config::Client driver>
 #                                               (defaults to Mongo)
 #                               --config_params <a JSON-encoded string of
-#                               parameters for the Mir::Config::Client object>
+#                                               parameters for the Mir::Config::Client object>
+#                               --log_config_params <a Log::Log4perl configuration script>
 #
 #  DESCRIPTION: This script implements the Mir::IR process.
 #
@@ -28,25 +29,32 @@ use JSON                    qw( decode_json );
 
 use Mir::IR ();
 
-Log::Log4perl->easy_init( $INFO );
-my $log = Log::Log4perl->get_logger( __PACKAGE__);
 
 my $campaign;
 my $config_driver = 'Mongo';
 my $config_params = { section => 'system' };
 my $config_params_json;
+my $log_config_params;
 
-GetOptions ("campaign=s"        => \$campaign,
-            "config_driver=s"   => \$config_driver,
-            "config_params=s"   => \$config_params_json,
+GetOptions ("campaign=s"            => \$campaign,
+            "config_driver=s"       => \$config_driver,
+            "config_params=s"       => \$config_params_json,
+            "log_config_params=s"   => \$log_config_params,
 ) or die("Error in command line arguments\n");
 die "
-Usage: $0 
- --campaign <campaign tag> \
- --config_driver  the Mir::Config::Client driver to use> (defaults to 'Mongo')
- --config_params  json-encoded string with the Mir::Config::Client driver specific params (defaults to section => 'system')
 
-At least a campaign \n" unless $campaign ;
+    Usage: $0 
+     --campaign <campaign tag> \
+     --config_driver     the Mir::Config::Client driver to use> (defaults to 'Mongo')
+     --config_params     json-encoded string with the Mir::Config::Client driver specific params (defaults to section => 'system')
+     --log_config_params path to a Log::Log4perl config file (if not provided logs will be directed to stdout)
+    
+    At least a campaign needs to be provided\n" 
+
+unless $campaign ;
+
+( $log_config_params ) ? Log::Log4perl->init( $log_config_params ) : Log::Log4perl->easy_init( $INFO );
+my $log = Log::Log4perl->get_logger( __PACKAGE__);
 
 $config_params = decode_json $config_params_json if ( $config_params_json );
 my $ir = Mir::IR->new(
