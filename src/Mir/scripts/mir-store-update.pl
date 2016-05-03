611f3d369b6662e57ab00082d152daa903a6d1aa
Marco Masetti <grubert65@gmail.com>
2016-05-03 10:19:47 +0200

    added

---
+++ src/Mir/scripts/mir-store-update.pl
@@ -0,0 +1,69 @@
+#!/usr/bin/env perl 
+#===============================================================================
+#
+#         FILE: mir-store-update-status.pl
+#
+#        USAGE: ./mir-store-update-status.pl  
+#                   --host       <MongoDB host> defaults to localhost
+#                   --database   <MongoDB database>
+#                   --collection <MongoDB collection>
+#                   --update     <update json string as '{"$set":{"field":"value"}}'
+#
+#
+#  DESCRIPTION: 
+#
+#      OPTIONS: ---
+#       AUTHOR: Marco Masetti (marco.masetti at softeco.it)
+# ORGANIZATION: 
+#      VERSION: 1.0
+#      CREATED: 05/03/2016 09:55:23 AM
+#     REVISION: ---
+#===============================================================================
+use strict;
+use warnings;
+use utf8;
+use Getopt::Long;
+use Mir::Store ();
+use JSON;
+use Log::Log4perl qw(:easy);
+Log::Log4perl->easy_init($DEBUG);
+
+my ($host, $database, $collection, $update_json) = ('localhost');
+
+GetOptions(
+    "host=s"    => \$host,
+    "database=s"=> \$database,
+    "collection=s"  => \$collection,
+    "update=s"      => \$update_json
+) or die <<EOT;
+
+        USAGE: ./mir-store-update-status.pl  
+                   --host       <MongoDB host> defaults to localhost
+                   --database   <MongoDB database>
+                   --collection <MongoDB collection>
+                   --update     <update json string as '{"\$set":{"field":"value"}}'
+
+EOT
+
+my $update = decode_json( $update_json );
+
+my $store = Mir::Store->create(
+    driver => 'MongoDB',
+    params => {
+        host        => $host,
+        database    => $database,
+        collection  => $collection
+}) or die "Error getting a Mir::Store object\n";
+
+$store->connect() or die "Error connecting to store\n";
+
+my $cursor = $store->find();
+
+while ( my $doc = $cursor->next ) {
+    unless ( $store->update( { '_id' => $doc->{_id} }, $update ) ) {
+        print "Error updating document  with _id: ".$doc->{_id}->{value}."\n";
+    }
+}
+
+
+
